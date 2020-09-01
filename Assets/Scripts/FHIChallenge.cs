5d2dd2c1b180df809c4aa302dbf9c665b255e3ed
GehadAbdElhalim <halimgego@gmail.com>
2020-09-02 01:06:10 +0200

    Added logic for Find hidden items

---
+++ Assets/Scripts/FHIChallenge.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class FHIChallenge : Challenge
+{
+    public OnItemChosenEvent OnItemChosen = new OnItemChosenEvent();
+
+    public List<ItemBehaviour> items;
+    public List<string> texts;
+
+    [Header("Duration of the challenge")]
+    public int minutes;
+    public int seconds;
+
+    ItemBehaviour currentItem;
+    string currentText;
+
+    void Start()
+    {
+        foreach(var x in items)
+        {
+            x.onItemClicked.AddListener(CheckForCorrectItem);
+        }
+
+        ChooseRandomObject();
+    }
+
+    void ChooseRandomObject()
+    {
+        int index = Random.Range(0, items.Count);
+        currentItem = items[index];
+        currentText = texts[index];
+        OnItemChosen.Invoke(currentText);
+    }
+
+    void CheckForCorrectItem(ItemBehaviour item)
+    {
+        if(item.GetInstanceID() == currentItem.GetInstanceID())
+        {
+            OnChallengeFinished.Invoke();
+        }
+    }
+
+    private void OnEnable()
+    {
+        TimerController.Instance.StartTimer(seconds, minutes, null, null);
+    }
+
+    public override void ResetProgress()
+    {
+        TimerController.Instance.StartTimer(seconds, minutes, null, null);
+    }
+}
+
+public class OnItemChosenEvent : UnityEvent<string>
+{
+
+}
---
+++ Assets/Scripts/ItemBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ItemBehaviour : MonoBehaviour, IPointerClickHandler
+{
+    public OnItemClickedEvent onItemClicked = new OnItemClickedEvent();
+
+    bool alreadyClicked;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (alreadyClicked)
+        {
+            return;
+        }
+
+        alreadyClicked = true;
+
+        onItemClicked.Invoke(this);
+    }
+
+    void Start()
+    {
+        alreadyClicked = false;
+    }
+}
+
+public class OnItemClickedEvent : UnityEvent<ItemBehaviour>
+{
+
+}
