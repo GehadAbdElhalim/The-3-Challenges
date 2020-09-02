9bd86643972cad873a0f8e18b3ad9b5538644e40
GehadAbdElhalim <halimgego@gmail.com>
2020-09-02 13:55:08 +0200

    Added the photos to find the hidden items

--- Assets/Scripts/ItemBehaviour.cs
+++ Assets/Scripts/ItemBehaviour.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ItemBehaviour : MonoBehaviour, IPointerClickHandler
 {
     public OnItemClickedEvent onItemClicked = new OnItemClickedEvent();
 
     bool alreadyClicked;
+
+    Image img;
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -19,11 +22,29 @@ public class ItemBehaviour : MonoBehaviour, IPointerClickHandler
 
         alreadyClicked = true;
 
+        StartCoroutine(Flash(2, 5, Color.green));
+    }
+
+    IEnumerator Flash(float duration, float frequency, Color color)
+    {
+        float totalDuration = duration;
+
+        while(duration > 0)
+        {
+            img.color = color;
+            yield return new WaitForSeconds(totalDuration / frequency);
+            img.color = Color.white;
+            yield return new WaitForSeconds(totalDuration / frequency);
+            duration -= 2 * totalDuration / frequency;
+        }
+
         onItemClicked.Invoke(this);
     }
 
     void Start()
     {
+        img = GetComponent<Image>();
+
         alreadyClicked = false;
     }
 }
