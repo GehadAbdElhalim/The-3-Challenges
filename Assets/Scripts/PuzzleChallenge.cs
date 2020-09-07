d723797efed1da058cf6bd62c71837b0867fd874
GehadAbdElhalim <halimgego@gmail.com>
2020-09-07 16:03:59 +0200

    added something

---
+++ Assets/Scripts/PuzzleChallenge.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleChallenge : Challenge
+{
+    public Image puzzlePiecePrefab;
+    public Transform parent;
+
+    private void Start()
+    {
+        MakeGrid(new Vector3(-566,295,0));
+    }
+
+    void MakeGrid(Vector3 startPos)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Instantiate(puzzlePiecePrefab, new Vector3(startPos.x + i * 100, startPos.y - j * 100, 0), Quaternion.identity, parent);
+            }
+        }
+    }
+
+    public override void ResetProgress()
+    {
+        
+    }
+}
