35d4bcf98f64c5b4c92a4107862ee4172a8614bf
GehadAbdElhalim <halimgego@gmail.com>
2020-09-30 13:16:09 +0200

    Fixed Find hidden items

--- Assets/Scripts/FHIChallenge.cs
+++ Assets/Scripts/FHIChallenge.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using RTLTMPro;
@@ -12,6 +13,7 @@ public class FHIChallenge : Challenge
     [Header("Specific parameters for this challenge")]
     public List<ItemBehaviour> items;
     public List<GameObject> texts;
+    public int numberOfHiddenItems;
 
     [Header("Duration of the challenge")]
     public int minutes;
@@ -22,8 +24,10 @@ public class FHIChallenge : Challenge
     public float frequency;
     public Color flashColor;
 
-    ItemBehaviour currentItem;
-    GameObject currentText;
+    int[] indicesOfChosenItems;
+    int indexOfIndicesOfChosenItems;
+    int currentItemIndex;
+    bool isFirstTimeDone = false;
 
     [Header("SFX")]
     public AudioClip correct_sfx;
@@ -36,29 +40,50 @@ public class FHIChallenge : Challenge
         {
             x.onItemClicked.AddListener(CheckForCorrectItem);
         }
-
-        ChooseRandomObject();
     }
 
     void ChooseRandomObject()
     {
-        int index = Random.Range(0, items.Count);
-        currentItem = items[index];
-        currentText = texts[index];
+        if (!isFirstTimeDone)
+        {
+            int[] arrayOfIndices = new int[items.Count];
+
+            for (int i = 0; i < arrayOfIndices.Length; i++)
+            {
+                arrayOfIndices[i] = i;
+            }
+
+            indicesOfChosenItems = ChooseRandomNElementsFromArray<int>(arrayOfIndices, numberOfHiddenItems);
+            isFirstTimeDone = true;
+        }
+
+        if(indexOfIndicesOfChosenItems < indicesOfChosenItems.Length)
+        {
+            currentItemIndex = indicesOfChosenItems[indexOfIndicesOfChosenItems++];
+            print(currentItemIndex);
+        }
+        else
+        {
+            OnChallengeFinished.Invoke();
+            return;
+        }
+
         ActivateCurrentText();
     }
 
     void CheckForCorrectItem(ItemBehaviour item)
     {
-        if(item.GetInstanceID() == currentItem.GetInstanceID())
+        if(item.GetInstanceID() == items[currentItemIndex].GetInstanceID())
         {
             audio.PlayOneShot(correct_sfx);
-            StartCoroutine(item.Flash(duration, frequency, flashColor, () => OnChallengeFinished.Invoke()));
+            StartCoroutine(item.Flash(duration, frequency, flashColor, ChooseRandomObject));
         }
     }
 
     private void OnEnable()
     {
+        indexOfIndicesOfChosenItems = 0;
+
         ChooseRandomObject();
 
         Invoke("StartTimer", Time.deltaTime);
@@ -74,6 +99,8 @@ public class FHIChallenge : Challenge
 
     public override void ResetProgress()
     {
+        isFirstTimeDone = false;
+        indexOfIndicesOfChosenItems = 0;
         DisableAllTexts();
         ChooseRandomObject();
         TimerController.Instance.StartTimer(seconds, minutes, null, null);
@@ -90,6 +117,36 @@ public class FHIChallenge : Challenge
     void ActivateCurrentText()
     {
         DisableAllTexts();
-        currentText.SetActive(true);
+        texts[currentItemIndex].SetActive(true);
+    }
+
+    /// <summary>
+    /// This method picks n Random elements from the array and returns an array with those elements
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="array"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    private T[] ChooseRandomNElementsFromArray<T>(T[] array, int n)
+    {
+        var randomObjects = new T[array.Length];
+
+        for (int i = 0; i < n; i++)
+        {
+            // Take only from the latter part of the list - ignore the first i items.
+            int take = UnityEngine.Random.Range(i, array.Length);
+            randomObjects[i] = array[take];
+
+            // Swap our random choice to the beginning of the array,
+            // so we don't choose it again on subsequent iterations.
+            array[take] = array[i];
+            array[i] = randomObjects[i];
+        }
+
+        T[] output = new T[n];
+
+        Array.Copy(randomObjects, 0, output, 0, output.Length);
+
+        return output;
     }
 }
