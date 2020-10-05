cfe9013e4c2b36f309fc8598774d129985b22f50
GehadAbdElhalim <halimgego@gmail.com>
2020-10-06 00:54:14 +0200

    Added some things

---
+++ Assets/ParticleSystemController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleSystemController : MonoBehaviour
+{
+    ParticleSystem particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    public void PlayParticlesAt(Transform pos)
+    {
+        transform.position = pos.position;
+        particles.Play();
+    }
+}
